43d7065e4202dc2356f317d289f7f4869aa7480b
KebingFang <Kebing-fang@outlook.com>
2021-08-19 19:55:59 -0700

    commit all

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,15 +1,24 @@
+from settings import Settings
 import sys
 import pygame
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, 
+                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
     
     while (True):
-        for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
-                sys.exit()
+        gf.check_events()
+        screen.fill(ai_settings.bg_color)
+        
+        ship.blitme()
         pygame.display.flip()
-run_game()         
+run_game()      
+
+
         
