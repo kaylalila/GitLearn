631ef43d28802bdaa6f77e2f202efedbb29649fe
KebingFang <Kebing-fang@outlook.com>
2021-08-19 19:56:23 -0700

    commit

---
+++ alien_invasion/settings.py
@@ -0,0 +1,8 @@
+# store all class in alien invasion game
+class Settings():
+    def __init__(self):
+        
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
