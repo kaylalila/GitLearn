b1afde1e8b1efb701752347b05fcac39fe3ebcae
KebingFang <Kebing-fang@outlook.com>
2021-08-25 21:19:26 -0700

    commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 from settings import Settings
 import sys
 import pygame
+from pygame.sprite import Group
 from ship import Ship
 import game_functions as gf
 
@@ -10,14 +11,20 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, 
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     
     while (True):
-        gf.check_events()
-        screen.fill(ai_settings.bg_color)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        for bullet in bullets.copy():
+            if (bullet.rect.bottom <= 0):
+                bullets.remove(bullet)
+            print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(ai_settings, screen, ship, bullets)
         
-        ship.blitme()
-        pygame.display.flip()
 run_game()      
 
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,5 +4,11 @@ class Settings():
         
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
-        
+        self.bg_color = (30, 50, 100)
+        self.ship_speed_factor = 1.5
+
+        self.bullet_speed_factor = 1
+        self.bullet_height = 15
+        self.bullet_width = 3
+        self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
